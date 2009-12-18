2d49d04b27a2dd61c918123b7164db2b43505b50
Elijah Miller <elijah.miller@gmail.com>
2009-12-18 11:06:35 +0800

    Intercepting log messages instead of parsing log files to drastically reduce memory consumption with large logs.
    Log intercept code pulled from http://github.com/brynary/rack-bug/

--- lib/rails-footnotes/notes/log_note.rb
+++ lib/rails-footnotes/notes/log_note.rb
@@ -3,34 +3,45 @@ require "#{File.dirname(__FILE__)}/abstract_note"
 module Footnotes
   module Notes
     class LogNote < AbstractNote
+      @@log = []
+      
+      def self.log(message)
+        @@log << message
+      end
+      
       def initialize(controller)
         @controller = controller
       end
 
-      def content
-        escape(log_tail).gsub("\n","<br />")
+      def title
+        "Log (#{log.count("\n")})"
       end
 
-      protected
-        def log_tail
-          filename = if RAILS_DEFAULT_LOGGER.instance_variable_get('@log')
-            RAILS_DEFAULT_LOGGER.instance_variable_get('@log').path
-          else 
-            RAILS_DEFAULT_LOGGER.instance_variable_get('@logdev').filename
-          end
-          file_string = File.open(filename).read.to_s
-
-          # We try to select the specified action from the log
-          # If we can't find it, we get the last 100 lines
-          #
-          if rindex = file_string.rindex('Processing '+@controller.controller_class_name+'#'+@controller.action_name)
-            file_string[rindex..-1].gsub(/\e\[.+?m/, '')
-          else
-            lines = file_string.split("\n")
-            index = [lines.size-100,0].max
-            lines[index..-1].join("\n")
+      def content
+        escape(log.gsub(/\e\[.+?m/, '')).gsub("\n", '<br />')
+      end
+      
+      def log
+        unless @log
+          @log = @@log.join('')
+          @@log = []
+          if rindex = @log.rindex('Processing '+@controller.controller_class_name+'#'+@controller.action_name)
+            @log = @log[rindex..-1]
           end
         end
+        @log
+      end
+
+      module LoggingExtensions
+        def add(*args, &block)
+          logged_message = super
+          Footnotes::Notes::LogNote.log(logged_message)
+          logged_message
+        end
+      end
+      
+      Rails.logger.extend LoggingExtensions
     end
   end
 end
+
