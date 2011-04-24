3ebb938545b1d58a828bff1192c4e7697c6bb2f4
Roman V. Babenko <romanvbabenko@gmail.com>
2011-04-24 10:26:07 +0300

    [^] Tests fixed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,4 @@ require 'active_support'
 require 'active_support/all' unless Class.respond_to?(:cattr_accessor)
 require 'rails-footnotes/footnotes'
 require 'rails-footnotes/notes/abstract_note'
+require "rails-footnotes"
