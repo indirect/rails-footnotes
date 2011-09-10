3174e14d3ceff8468b40c790e5f76a445ae6af4f
Roman V. Babenko <romanvbabenko@gmail.com>
2011-09-10 09:01:11 +0300

    [+] rails_footnotes install generator

---
+++ lib/generators/rails_footnotes/install_generator.rb
@@ -0,0 +1,17 @@
+module RailsFootnotes
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      desc "Creates and copy RailsFootnotes default files to your application."
+
+      def copy_initializer
+        template "rails_footnotes.rb", "config/initializers/rails_footnotes.rb"
+      end
+
+      def copy_dotfile
+        template ".rails_footnotes", ".rails_footnotes"
+      end
+    end
+  end
+end
