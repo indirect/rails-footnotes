cb0ea3d2a3587fbadc5ed4d461616effb55ada69
Intrepidd <adrien@siami.fr>
2014-03-03 00:16:46 +0100

    Remove some rails 2.x specific code and update readme concerning rails 2.x

--- lib/rails-footnotes/filter.rb
+++ lib/rails-footnotes/filter.rb
@@ -102,16 +102,11 @@ module Footnotes
       end
 
       def performed_render?
-        @controller.instance_variable_get(:@performed_render) || # rails 2.x
-          (@controller.respond_to?(:performed?) && @controller.performed?) # rails3, will break on redirect??
+        @controller.respond_to?(:performed?) && @controller.performed?
       end
 
       def valid_format?
-        if @template # Rails 2.x
-          [:html,:rhtml,:xhtml,:rxhtml].include?(@template.send(template_format_method.to_sym).to_sym)
-        else # Rails 3
-          @controller.response.content_type == 'text/html'
-        end
+        @controller.response.content_type == 'text/html'
       end
 
       def template_format_method
