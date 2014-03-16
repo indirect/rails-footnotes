55364001d3fa5ded9d250d9ee47e4c9238177e74
Intrepidd <adrien@siami.fr>
2014-03-16 11:15:58 +0100

    Try to make the note logger work with rails 3 and rails 4

--- lib/rails-footnotes/notes/log_note/note_logger.rb
+++ lib/rails-footnotes/notes/log_note/note_logger.rb
@@ -16,6 +16,43 @@ module Footnotes
           @logs << formatter.call(format_severity(severity), Time.now, message, progname)
         end
 
+        ## Backport from rails 4 for handling logging broadcast, should be removed when rails 3 is deprecated :
+
+        # Broadcasts logs to multiple loggers.
+        def self.broadcast(logger) # :nodoc:
+          Module.new do
+            define_method(:add) do |*args, &block|
+              logger.add(*args, &block)
+              super(*args, &block)
+            end
+
+            define_method(:<<) do |x|
+              logger << x
+              super(x)
+            end
+
+            define_method(:close) do
+              logger.close
+              super()
+            end
+
+            define_method(:progname=) do |name|
+              logger.progname = name
+              super(name)
+            end
+
+            define_method(:formatter=) do |formatter|
+              logger.formatter = formatter
+              super(formatter)
+            end
+
+            define_method(:level=) do |level|
+              logger.level = level
+              super(level)
+            end
+          end
+        end
+
       end
     end
   end
