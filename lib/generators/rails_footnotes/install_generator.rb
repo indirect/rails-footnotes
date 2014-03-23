8b538a4992910c0f0537fcd4f2bf492f0e8f3f79
Intrepidd <adrien@siami.fr>
2014-03-23 11:33:51 +0100

    Remove .rails_footnotes from generated files since it is not used anymore

--- lib/generators/rails_footnotes/install_generator.rb
+++ lib/generators/rails_footnotes/install_generator.rb
@@ -3,15 +3,12 @@ module RailsFootnotes
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../../templates", __FILE__)
 
-      desc "Creates and copy RailsFootnotes default files to your application."
+      desc "Copy rails-footnotes initializer to your application."
 
       def copy_initializer
         template "rails_footnotes.rb", "config/initializers/rails_footnotes.rb"
       end
 
-      def copy_dotfile
-        template "rails_footnotes", ".rails_footnotes"
-      end
     end
   end
 end
