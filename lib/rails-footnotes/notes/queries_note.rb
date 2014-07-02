8ff477e6ed2afca16b27b02ba7afe48b1795c5d7
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2014-07-02 08:22:07 +0300

    Closes #112 Initializer needs to call super() first in order for patters to be initialized to an empty array

--- lib/rails-footnotes/notes/queries_note.rb
+++ lib/rails-footnotes/notes/queries_note.rb
@@ -103,6 +103,7 @@ module Footnotes
       attr_accessor :events, :ignore_regexps
 
       def initialize(orm)
+        super() 
         @events = []
         orm.each {|adapter| ActiveSupport::LogSubscriber.attach_to adapter, self}
       end
