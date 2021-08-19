bf67bf2eeab5a39da0d9f7c6a3a808bff3575fab
Andre Arko <andre@arko.net>
2021-08-19 02:03:20 -0700

    remove support for Rails <6

--- lib/rails-footnotes/extension.rb
+++ lib/rails-footnotes/extension.rb
@@ -2,31 +2,23 @@ require 'active_support/concern'
 
 module Footnotes
   module RailsFootnotesExtension
-
     extend ActiveSupport::Concern
 
     included do
-      if Rails::VERSION::MAJOR >= 5
-        prepend_before_action :rails_footnotes_before_filter
-        after_action :rails_footnotes_after_filter
-      else
-        prepend_before_filter :rails_footnotes_before_filter
-        after_filter :rails_footnotes_after_filter
-      end
+      prepend_before_action :rails_footnotes_before_filter
+      after_action :rails_footnotes_after_filter
     end
 
     def rails_footnotes_before_filter
-      if Footnotes.enabled?(self)
-        Footnotes::Filter.start!(self)
-      end
+      Footnotes::Filter.start!(self) if Footnotes.enabled?(self)
     end
 
     def rails_footnotes_after_filter
-      if Footnotes.enabled?(self)
-        filter = Footnotes::Filter.new(self)
-        filter.add_footnotes!
-        filter.close!(self)
-      end
+      return unless Footnotes.enabled?(self)
+
+      filter = Footnotes::Filter.new(self)
+      filter.add_footnotes!
+      filter.close!(self)
     end
   end
 end
