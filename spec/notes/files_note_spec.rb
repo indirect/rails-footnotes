7afa99164b59bb552f2d1fe3fae10fbcbedc9313
Andre Arko <andre@arko.net>
2021-08-19 21:21:59 -0700

    test a concrete filesnote

--- spec/notes/files_note_spec.rb
+++ spec/notes/files_note_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 require 'action_controller'
 require "rails-footnotes/notes/files_note"
 
+class ConcreteFilesNote < Footnotes::Notes::FilesNote
+  def scan_text(text)
+    []
+  end
+end
+
 describe Footnotes::Notes::FilesNote do
 
   let(:note) do
-    Footnotes::Notes::FilesNote.new(double('controller', :response => double('', :body => '')))
+    ConcreteFilesNote.new(double('controller', :response => double('', :body => '')))
   end
 
   subject { note }
