9868eef57bab53d249b6536215370c1df8732c67
Andre Arko <andre@arko.net>
2021-08-19 21:03:13 -0700

    remove deprecated run!

--- lib/rails-footnotes.rb
+++ lib/rails-footnotes.rb
@@ -5,7 +5,6 @@ require 'rails-footnotes/each_with_rescue'
 require 'rails-footnotes/filter'
 require 'rails-footnotes/notes/all'
 require 'rails-footnotes/extension'
-require 'active_support/deprecation'
 
 module Footnotes
   mattr_accessor :before_hooks
@@ -35,11 +34,6 @@ module Footnotes
 
     delegate :font_size, :to => Filter
     delegate :font_size=, :to => Filter
-  end
-
-  def self.run!
-    ActiveSupport::Deprecation.warn "run! is deprecated and will be removed from future releases, use Footnotes.setup or Footnotes.enabled instead.", caller
-    Footnotes.enabled = true
   end
 
   def self.before(&block)
